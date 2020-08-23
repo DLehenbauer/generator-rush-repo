199b0937257b8c5b4f1fad9159b8ea3f2eb1f4ae
DLehenbauer <DLehenbauer@users.noreply.github.com>
2020-08-23 18:25:23 +0000

    ESLint: Add 'eslint-plugin-header'

--- generators/app/templates/packages/common/config/eslint-config/index.js
+++ generators/app/templates/packages/common/config/eslint-config/index.js
@@ -2,5 +2,9 @@
 require('@rushstack/eslint-config/patch/modern-module-resolution');
  
 module.exports = {
-    extends: [ "@rushstack/eslint-config" ],
+    extends: ["@rushstack/eslint-config"],
+    plugins: ["header"],
+    rules: {
+        "header/header": [2, "block", "!\n * Copyright (c) <%= author %>. All rights reserved.\n * Licensed under the <%= license %> License.\n "]
+    },
 };
--- generators/app/templates/packages/example-typescript/src/index.ts
+++ generators/app/templates/packages/example-typescript/src/index.ts
@@ -1,3 +1,8 @@
+/*!
+ * Copyright (c) <%= author %>. All rights reserved.
+ * Licensed under the <%= license %> License.
+ */
+
 export function example(): number {
     return Math.random();
 }
