72448b81e62c60c0116fc21f0f9a0462ce16f16d
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-01-21 18:23:27 +0000

    Ts-Jest no longer needs/supports 'packageJson'

--- generators/app/templates/packages/common/config/jest-config/index.js
+++ generators/app/templates/packages/common/config/jest-config/index.js
@@ -1,14 +1,8 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+    preset: "ts-jest",
+    testEnvironment: "node",
 
     // Workaround 'jest' writing test results to stderr, even on success.
     // (See: https://github.com/facebook/jest/issues/5064)
     reporters: ["jest-standard-reporter"],
-
-    // Work around issue locating package root when 'ts-jest' is symlinked.
-    // (See: https://github.com/kulshekhar/ts-jest/issues/823)
-    globals: {
-        'ts-jest': { packageJson: 'package.json', },
-    },
 };
