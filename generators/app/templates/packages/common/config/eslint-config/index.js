99728e2b0b44f907e239d338e73dec98f1c96a03
Daniel Lehenbauer <DLehenbauer@users.noreply.github.com>
2021-01-21 19:04:36 +0000

    Add eslint-plugin-prettier

--- generators/app/templates/packages/common/config/eslint-config/index.js
+++ generators/app/templates/packages/common/config/eslint-config/index.js
@@ -2,8 +2,8 @@
 require("@rushstack/eslint-config/patch/modern-module-resolution");
 
 module.exports = {
-    extends: ["@rushstack/eslint-config"],
-    plugins: ["header"],
+    extends: ["@rushstack/eslint-config/profile/node"],
+    plugins: ["header", "prettier"],
     rules: {
         "header/header": [
             "error",
@@ -11,5 +11,6 @@ module.exports = {
             "!\n * Copyright (c) <%= author %>. All rights reserved.\n * Licensed under the <%= license %> License.\n ",
             /* newLines: */ 2, // '\n\n' = 1 blank line after block comment
         ],
+        "prettier/prettier": "error",
     },
 };
